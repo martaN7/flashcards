912469a6de2e0f094b529acebd24658cf04b22d8
Marta Stefaniak <vocado7@gmail.com>
2023-11-04 20:51:48 +0100

    setup react-router-dom v6 with outlet for layout

---
+++ src/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/Layout.jsx";
+import Decks from "./components/decks/Decks.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Decks />,
+      },
+    ],
+  },
+]);
+gs;
+export default router;
